1d47ffdad6ea52d96cbe892cf8571cfa63b23f62
Atibway <ainebyoonaatiidu@gmail.com>
2024-11-22 12:38:01 +0300

    ok

--- actions/settings.ts
+++ actions/settings.ts
@@ -1,9 +1,9 @@
 "use server"
-import { getUserByEmail, getUserById } from "@/data/user";
+import {  getUserById } from "@/data/user";
 import { currentUser } from "@/lib/auth"
 import { db } from "@/lib/prismadb";
 import { SettingsSchema } from "@/schemas"
-import bcrypt from "bcryptjs"
+
 import * as z from "zod"
 
 
@@ -19,14 +19,6 @@ const dbUser = await getUserById(user.id)
 if(!dbUser) {
     return {error: "Unauthorized"}
 }
-
-if(user.isOAuth){
-values.email = undefined;
-values.password = undefined;
-values.newPassword = undefined;
-values.isTwoFactorEnabled = undefined;
-}
-
 
 
 
