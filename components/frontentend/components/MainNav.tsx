77c700946933e42d4b160ffb0d7e2e1b01fda2a8
Atibway <ainebyoonaatiidu@gmail.com>
2024-11-29 11:23:16 +0300

    k

--- components/frontentend/components/MainNav.tsx
+++ components/frontentend/components/MainNav.tsx
@@ -30,9 +30,11 @@ const MainNav: React.FC<MainNavProps> = ({
     >
       <div className="hidden md:block">
       {routes.map((route)=> (
-        <div className="flex gap-x-2">
- <Link
+        <div
         key={route.href}
+        className="flex gap-x-2">
+ <Link
+        
         href={route.href}
         className={cn(
           "text-sm font-medium   transition-colors hover:text-black",
