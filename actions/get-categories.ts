2d9aa2b271874362edb367c06fe8a1a05d30f00e
Atibway <ainebyoonaatiidu@gmail.com>
2026-05-09 09:33:00 +0300

    gg

--- actions/get-categories.ts
+++ actions/get-categories.ts
@@ -1,14 +1,22 @@
-import { Category } from "@/types";
-import axios from 'axios';
+import { db } from "@/lib/prismadb";
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
-
-const getCategories = async (): Promise<Category[]> => {
-    // const res = await fetch(URL)
-    const response = await axios.get(URL);
-   
-    // return res.json()
-    return response.data;
+const getCategories = async () => {
+    try {
+        const categories = await db.category.findMany({
+            include: {
+                billboard: {
+                    include: {
+                        BillboardImages: true,
+                    },
+                },
+                subcategories: true,
+            },
+        });
+        return categories;
+    } catch (error) {
+        console.error('Error fetching categories:', error);
+        return [];
+    }
 }
 
 export default getCategories;
